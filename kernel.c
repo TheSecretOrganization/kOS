1186eaa7f0ea19d88f74c90d74ce4c40f80a6f5a
Antoine Verin <24255551+antoineverin@users.noreply.github.com>
2025-01-21 10:32:46 +0100

    fix coding style

--- kernel.c
+++ kernel.c
@@ -6,8 +6,7 @@ const size_t VGA_HEIGHT = 25;
 
 uint16_t *term_buf;
 
-void init_term()
-{
+void init_term() {
 	term_buf = (uint16_t*) 0xB8000;
 
 	size_t buf_size = VGA_WIDTH * VGA_HEIGHT;
@@ -15,8 +14,7 @@ void init_term()
 		term_buf[i] = (uint16_t) ' ' | (uint16_t) (7 | 0 << 4) << 8;
 }
 
-void kernel_main()
-{
+void kernel_main() {
 	init_term();
 	term_buf[0] = (uint16_t) '4' | (uint16_t) (7 | 0 << 4) << 8;
 	term_buf[1] = (uint16_t) '2' | (uint16_t) (7 | 0 << 4) << 8;
