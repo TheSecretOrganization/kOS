aa29624be617c0cd597818602e2ab5ef461a1fed
Antoine Verin <24255551+antoineverin@users.noreply.github.com>
2025-01-22 12:36:01 +0100

    add utilitary function for printing
    * add term_putchar, putchar_at, putstr
    * add vga_value
    * init terminal variables

--- kernel.c
+++ kernel.c
@@ -1,21 +1,70 @@
 #include <stddef.h>
 #include <stdint.h>
 
+enum vga_color {
+	VGA_COLOR_BLACK = 0,
+	VGA_COLOR_BLUE = 1,
+	VGA_COLOR_GREEN = 2,
+	VGA_COLOR_CYAN = 3,
+	VGA_COLOR_RED = 4,
+	VGA_COLOR_MAGENTA = 5,
+	VGA_COLOR_BROWN = 6,
+	VGA_COLOR_LIGHT_GREY = 7,
+	VGA_COLOR_DARK_GREY = 8,
+	VGA_COLOR_LIGHT_BLUE = 9,
+	VGA_COLOR_LIGHT_GREEN = 10,
+	VGA_COLOR_LIGHT_CYAN = 11,
+	VGA_COLOR_LIGHT_RED = 12,
+	VGA_COLOR_LIGHT_MAGENTA = 13,
+	VGA_COLOR_LIGHT_BROWN = 14,
+	VGA_COLOR_WHITE = 15,
+};
+
 const size_t VGA_WIDTH = 80;
 const size_t VGA_HEIGHT = 25;
 
+size_t term_row;
+size_t term_column;
 uint16_t *term_buf;
 
+uint16_t vga_value(unsigned char c, enum vga_color fg, enum vga_color bg) {
+	return (uint16_t) c | (uint16_t) (fg | bg << 4) << 8;
+}
+
 void init_term() {
+	term_row = 0;
+	term_column = 0;
 	term_buf = (uint16_t*) 0xB8000;
 
 	size_t buf_size = VGA_WIDTH * VGA_HEIGHT;
 	for (size_t i = 0; i < buf_size; i++)
-		term_buf[i] = (uint16_t) ' ' | (uint16_t) (7 | 0 << 4) << 8;
+		term_buf[i] = vga_value(' ', VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+}
+
+void term_putchar_at(unsigned char c, size_t x, size_t y) {
+	size_t index = y * VGA_WIDTH + x;
+	term_buf[index] = vga_value(c, VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+}
+
+void term_putchar(unsigned char c) {
+	if (c != '\n')
+		term_putchar_at(c, term_column, term_row);
+	if (++term_column == VGA_WIDTH || c == '\n') {
+		term_column = 0;
+		if (++term_row == VGA_HEIGHT) {
+			term_row -= 1;
+		}
+	}
+}
+
+void term_putstr(char *str) {
+	size_t i = -1;
+
+	while (str[++i])
+		term_putchar(str[i]);
 }
 
 void kernel_main() {
 	init_term();
-	term_buf[0] = (uint16_t) '4' | (uint16_t) (7 | 0 << 4) << 8;
-	term_buf[1] = (uint16_t) '2' | (uint16_t) (7 | 0 << 4) << 8;
+	term_putstr("42");
 }
