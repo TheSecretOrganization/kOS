e6383ab61c842d945118017f3755749f14389461
Antoine <24255551+antoineverin@users.noreply.github.com>
2025-01-30 10:15:41 +0100

    replace as with nasm

--- boot.s
+++ boot.s
@@ -1,29 +1,27 @@
-.set MAGIC,		0x1BADB002
-.set FLAGS,		1<<0 | 1<<1
-.set CHECKSUM,	-(MAGIC + FLAGS)
+MAGIC		equ 0x1BADB002
+FLAGS		equ 1<<0 | 1<<1
+CHECKSUM	equ -(MAGIC + FLAGS)
 
-.section .multiboot
-.align 4
-.long MAGIC
-.long FLAGS
-.long CHECKSUM
+section .multiboot
+align 4
+dd MAGIC
+dd FLAGS
+dd CHECKSUM
 
-.section .bss
-.align 16
+section .bss
+align 16
 stack_bottom:
-.skip 16384
+	resb 16384
 stack_top:
 
-.section .text
-.global _start
-.type _start, @function
+section .text
+global _start
+extern kernel_main
 
 _start:
-	mov $stack_top, %esp
+	mov esp, stack_top
 	call kernel_main
 	cli
 loop:
 	hlt
 	jmp loop
-
-.size _start, . - _start
