2c10fa308a11ac33ae5337e111eca2b75c87f104
Antoine <24255551+antoineverin@users.noreply.github.com>
2025-02-11 15:03:26 +0100

    add missing attribute for isr handlers

--- kernel/src/idt/isr.c
+++ kernel/src/idt/isr.c
@@ -1,8 +1,8 @@
 #include "isr.h"
 #include "tty.h"
 
-void isr_exception_handler(
-	__attribute__((unused)) struct interrupt_frame* frame) {
+__attribute__((interrupt)) void
+isr_exception_handler(__attribute__((unused)) struct interrupt_frame* frame) {
 	tty_putstr("Exception occurred\n");
 	asm volatile("cli; hlt");
 }
