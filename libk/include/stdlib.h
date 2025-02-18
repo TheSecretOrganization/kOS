a1813265200c8057cdfb51e10ddf473c7a2eb8f1
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-02-18 14:41:25 +0100

    create strcmp and strncmp

---
+++ libk/include/stdlib.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int atoi(const char* nptr);
+
+#ifdef __cplusplus
+}
+#endif
--- libk/include/string.h
+++ libk/include/string.h
@@ -14,6 +14,8 @@ void* memmove(void* dest, const void* src, size_t n);
 void* memrchr(const void* s, int c, size_t n);
 void* memset(void* s, int c, size_t n);
 char* strchr(const char* s, int c);
+int strcmp(const char* s1, const char* s2);
+size_t strlcpy(char* dst, const char* src, size_t size);
 size_t strlen(const char*);
 int strncmp(const char* s1, const char* s2, size_t n);
 char* strrchr(const char* s, int c);
