946dee1f5a4c67733aed2848d78b40c8767143cd
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-02-18 15:31:40 +0100

    implement ctype functions

---
+++ libk/include/ctype.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int isalnum(int c);
+int isalpha(int c);
+int isdigit(int c);
+int islower(int c);
+int isprint(int c);
+int isspace(int c);
+int isupper(int c);
+int isascii(int c);
+int tolower(int c);
+int toupper(int c);
+
+#ifdef __cplusplus
+}
+#endif
