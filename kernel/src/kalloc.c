260c522f15b2f1fecb425f253ce15e18940ad464
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-04-25 15:52:26 +0200

    create bitmap page frame allocator

---
+++ kernel/src/kalloc.c
@@ -0,0 +1,42 @@
+#include "kalloc.h"
+#include <stdint.h>
+
+pageframe_t frame_map[BITMAP_SIZE] = {0};
+pageframe_t pre_frames[20] = {0};
+
+static pageframe_t kalloc_frame_int() {
+	uint32_t i = 0;
+	while (frame_map[i] != FREE) {
+		i++;
+		if (i == BITMAP_SIZE)
+			return (ERROR);
+	}
+	frame_map[i] = USED;
+	return (STARTFRAME + (i * 0x1000));
+}
+
+pageframe_t kalloc_frame() {
+	static bool allocate = true;
+	static uint8_t pframe = 0;
+	pageframe_t ret;
+
+	if (pframe == 20)
+		allocate = true;
+
+	if (allocate) {
+		allocate = false;
+		pframe = 0;
+		for (int i = 0; i < 20; i++)
+			pre_frames[i] = kalloc_frame_int();
+	}
+
+	ret = pre_frames[pframe];
+	pframe++;
+	return ret;
+}
+
+void kfree_frame(pageframe_t frame) {
+	uint32_t offset = frame - STARTFRAME;
+	uint32_t index = offset == 0 ? offset : offset / 0x1000;
+	frame_map[index] = FREE;
+}
