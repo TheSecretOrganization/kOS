7d081c0eef9804c074c320b3ebff5d1dfd8f5147
Andreï Basdereff <114996380+Neffi42@users.noreply.github.com>
2025-07-15 10:24:44 +0200

    Update debug tools (#99)

--- debug.sh
+++ debug.sh
@@ -3,37 +3,43 @@
 set -e
 
 usage() {
-  echo "Usage: $0 <kernel.bin>"
+    echo "Usage: $0 <kernel.bin> [breakpoint]"
 }
 
 while getopts ":h" opt; do
-  case $opt in
-  h)
-    usage
-    exit 0
-    ;;
-  \?)
-    echo "Invalid option: -$OPTARG"
-    exit 1
-    ;;
-  esac
+    case $opt in
+    h)
+        usage
+        exit 0
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG"
+        exit 1
+        ;;
+    esac
 done
 
 if [[ -z $1 ]]; then
-  usage
-  exit 1
+    usage
+    exit 1
+else
+    kernel_bin="$1"
 fi
 
-kernel_bin="$1"
+if [[ -z $2 ]]; then
+    breakpoint="kernel_main"
+else
+    breakpoint="$2"
+fi
 
 if command -v pwndbg >/dev/null 2>&1; then
-  dbg=pwndbg
+    dbg=pwndbg
 elif command -v gdb >/dev/null 2>&1; then
-  dbg=gdb
+    dbg=gdb
 else
-  echo "Please install pwndbg or gdb"
-  exit 1
+    echo "Please install pwndbg or gdb"
+    exit 1
 fi
 
 qemu-system-i386 -kernel "$kernel_bin" -no-reboot -S -s &
-$dbg -ex "target remote localhost:1234" -ex "break kernel_main" -ex "continue" "$kernel_bin"
+$dbg -ex "target remote localhost:1234" -ex "break $breakpoint" -ex "continue" "$kernel_bin"
