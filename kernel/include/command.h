5fc4fc5bac93c1e86d562605c7e42eb9a184d5b6
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-09-24 10:34:30 +0200

    add clear and default command

--- kernel/include/command.h
+++ kernel/include/command.h
@@ -3,3 +3,4 @@
 void cmd_handle(const char* command);
 void cmd_ping();
 void cmd_crash();
+void cmd_clear();
--- kernel/src/command/handle.c
+++ kernel/src/command/handle.c
@@ -1,10 +1,16 @@
 #include "command.h"
+#include <stdio.h>
 #include <string.h>
 
 void cmd_handle(const char* command) {
-	if (strcmp(command, "ping") == 0)
+	if (strcmp(command, "ping") == 0) {
 		cmd_ping();
-	if (strcmp(command, "crash") == 0) {
+	} else if (strcmp(command, "crash") == 0) {
 		cmd_crash();
+	} else if (strcmp(command, "clear") == 0) {
+		cmd_clear();
+	} else {
+		if (strlen(command) > 0)
+			printf("%s: command not found\n", command);
 	}
 }
