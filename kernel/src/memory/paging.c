b4ef25b06600c11834f58a2cafee2c6a9870af24
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-10-10 15:42:10 +0200

    pmm v2

--- kernel/src/memory/paging.c
+++ kernel/src/memory/paging.c
@@ -1,20 +1,24 @@
 #include "paging.h"
-#include "frame.h"
-#include <stddef.h>
+#include "pmm.h"
 #include <stdint.h>
-#include <stdio.h>
 
 extern void enable_paging(uint32_t pd);
 
-void paging_setup() {
-	uint32_t* pd = (uint32_t*)alloc_frame();
-	uint32_t* first_page_table = (uint32_t*)alloc_frame();
+void paging_init() {
+	pageframe_t pd_phys_addr = pmm_alloc_frame();
+	pageframe_t first_pt_phys_addr = pmm_alloc_frame();
 
-	for (size_t i = 0; i < ENTRIES_PER_TABLE; i++) {
-		first_page_table[i] = (i * PAGE_SIZE) | PAGE_PRESENT | PAGE_RW;
+	uint32_t* pd = (uint32_t*)pd_phys_addr;
+	uint32_t* first_pt = (uint32_t*)first_pt_phys_addr;
+
+	uint32_t bitmap_end_addr = get_bitmap_end_addr();
+
+	for (uint32_t addr = 0; addr < bitmap_end_addr; addr += PAGE_SIZE) {
+		first_pt[addr / PAGE_SIZE] = addr | PAGE_PRESENT | PAGE_RW;
 	}
 
-	pd[0] = (uint32_t)first_page_table | PAGE_PRESENT | PAGE_RW;
-	pd[1023] = (uint32_t)pd | PAGE_PRESENT | PAGE_RW;
-	enable_paging((uint32_t)pd);
+	pd[0] = first_pt_phys_addr | PAGE_PRESENT | PAGE_RW;
+	pd[1023] = pd_phys_addr | PAGE_PRESENT | PAGE_RW;
+
+	enable_paging(pd_phys_addr);
 }
