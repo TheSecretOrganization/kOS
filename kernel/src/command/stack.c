899c33538893d6d2e501ee43caee2d98b3c3b491
Antoine Verin <24255551+antoineverin@users.noreply.github.com>
2025-10-13 10:55:11 +0200

    remove unseen printf

--- kernel/src/command/stack.c
+++ kernel/src/command/stack.c
@@ -1,13 +1,11 @@
 #include "command.h"
 #include "utils.h"
 #include <stdint.h>
-#include <stdio.h>
 
 extern uint32_t stack_top;
 extern uint32_t stack_bottom;
 
 void cmd_stack() {
-	printf("bot %x top %x\n", (void*)&stack_bottom, (void*)&stack_top);
 	*(&stack_bottom + 15) = 0x42; // debug value
 	print_stack(&stack_bottom, &stack_top, 24);
 }
