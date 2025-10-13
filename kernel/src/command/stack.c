0ab32f49787f69afcce8e0de46f5c767591f6bbc
Antoine Verin <24255551+antoineverin@users.noreply.github.com>
2025-10-13 10:51:44 +0200

    remove redundant cast

--- kernel/src/command/stack.c
+++ kernel/src/command/stack.c
@@ -8,6 +8,6 @@ extern uint32_t stack_bottom;
 
 void cmd_stack() {
 	printf("bot %x top %x\n", (void*)&stack_bottom, (void*)&stack_top);
-	*((uint32_t*)&stack_bottom + 15) = 0x42; // debug value
-	print_stack((uint32_t*)&stack_bottom, (uint32_t*)&stack_top, 24);
+	*(&stack_bottom + 15) = 0x42; // debug value
+	print_stack(&stack_bottom, &stack_top, 24);
 }
